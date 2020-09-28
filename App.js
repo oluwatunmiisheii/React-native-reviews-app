5aaf2abf4b3cfae6340b3b080d0fb4cf5652bfdf
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2020-09-28 03:35:51 +0100

    chore: added custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
 import Home from './screens/home';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
-  return (
-    <Home />
-  );
+  let [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Home />
+    );
+  }
 }
+
 
